8970d35312778f81dbb5f9d3fa2e462a1cff0226
Nima Khastar <nimapizll.nn@gmail.com>
2024-05-11 20:13:31 +0330

    fix:Make the rgbcolor function code
    Making the code importing rgb colors in rgbcolor function

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-// //
-// let body = document.body;
-// let btncolor = document.querySelector("#btncolor");
+//
+let body = document.body;
+let btncolor = document.querySelector("#btncolor");
 
-// //
-// btncolor.addEventListener("click", rgbcolor);
+//
+btncolor.addEventListener("click", rgbcolor);
 
-// //  function  random nubmer
+//  function  random nubmer
 
-// function randomball() {
-//   return Math.floor(Math.random() * 255)
-// }
+function randomball() {
+  return Math.floor(Math.random() * 255);
+}
 
-// function rgbcolor() {
-//   let rgb1color = "rgb( ${randomball()},${randomball()},${randomball()})";
-//   body.style.backgroundColor = rgb1color;
-// }
-let btncolor =document.querySelector("btncolor")
-const btn = document.getElementById("btncolor");
+function rgbcolor() {
+  let rgb1color =`rgb(${randomball()},${randomball()},${randomball()})`;
+  body.style.backgroundColor = rgb1color;
+}
 
-let rgbrandom = () => {
-  return Math.floor(Math.random() * 256);
-};
-let changcolor = () => {
-  console.log("Function called");
-};
-// btncolor.addEventListener ("click",changcolor)
